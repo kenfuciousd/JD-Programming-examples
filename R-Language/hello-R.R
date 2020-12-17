3196a16eb780ed5e6ae417dea52ba341c85a8c0c
kenfuciousd <35818192+kenfuciousd@users.noreply.github.com>
2020-12-17 13:16:21 -0600

    R
    updates from intro to Data Science Tools

---
+++ R-Language/hello-R.R
@@ -0,0 +1,11 @@
+library(datasets)
+data(iris)
+View(iris) 
+
+unique(iris$Species)
+
+
+library(GGally)
+
+ggpairs(iris, mapping=ggplot2::aes(colour = Species))
+
---
+++ R-Language/shiny_assignment.R
@@ -0,0 +1,43 @@
+#To install the packages, copy, paste and execute the following command into the bottom left Console window.
+install.packages("shiny")
+install.packages("leaflet")
+
+#Paste the following code into a new R edit window in RStudio:
+library(shiny)
+library(leaflet)
+
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+names(r_colors) <- colors()
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+  actionButton("recalc", "New points"),
+  p(),
+  textOutput("coordinates")
+)
+
+server <- function(input, output, session) {
+  points <- eventReactive(input$recalc, {
+    points = cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+    output$coordinates <- renderText({ 
+      points
+    })
+    return(points)}, ignoreNULL = FALSE)
+  
+  observeEvent(input$Map_shape_click, { # update the location selectInput on map clicks
+    output$coordinates <- renderText({ 
+      "You have selected this"
+    })
+  })
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = points())
+  })
+  
+}
+
+shinyApp(ui, server)
