a1cef6d8c24938ca028593df330afaeaaceaf638
kenfuciousd <35818192+kenfuciousd@users.noreply.github.com>
2022-06-23 15:41:33 -0500

    updated win algorithm and logic
    .

--- Python/math_slot_simulator/mathsim.py
+++ Python/math_slot_simulator/mathsim.py
@@ -1,7 +1,12 @@
 # mathsim.py
 # 
 # requirements: 
-#   input: excel file info here
+#   input: excel file with three sheets: 'Reels', "Paytable", ""Paylines"
+#		Reels should have "Reel label" in row 1, and then each reel should have a set of symbols simulating the reel strip
+#			each line should have only a one symbol abbreviation, reflected in the paytable
+#		Paytable should have a "Reel label" in row 1, then each row should be a winning combonation
+#		Paylines should have each payline with a parenthesis set of numbers, (reel,game-window-position), starting from zero. 
+#			like: (0,1) | (1,2) | (2,1) which would look like a downward pointing arrow shape from the second row - _ -
 # 
 # python packages installed: openpyxl, (vaex?)
 
@@ -16,12 +21,11 @@ class SlotMachine:
 	"""Slot machine class, takes in the 'input' file as well as the reel # configured """ 
 
 # initialize:
-	def __init__(self,filepath, reels, paylines, bet):
+	def __init__(self,filepath, reels, paylines, bet, initial_credits):
 		self.filepath = filepath
 		self.reels = reels
 		self.paylines = paylines
-		# the "wallet"
-		credits = 0
+		self.credits = initial_credits 
 		self.bet = bet
 		#initialize data tp be used in the local object namespace, so it's able to be referenced. 
 		game_window = []
@@ -45,7 +49,6 @@ class SlotMachine:
 		reel2 = reel_data['Reel 2']
 		reel3 = reel_data['Reel 3']
 		# set random reel positions
-		randomize()
 
 	  	#if(reels==5):
 		# 	reel4 = reel_data['Reel 4']
@@ -53,24 +56,31 @@ class SlotMachine:
 		#  	reel5 = reel_data['Reel 5']	
 	 	#	reel5pos=random.randint(0,len(reel5)-1)
 
+	 	# chooses the random positions for each of the reels, eventually add the total reel number to pass
+		randomize()
+
 		# 	load paytables
 		paytable_data = pd.read_excel(filepath, sheet_name='Paytable')
+		# this is where we will call the paytable loader with the data
+		build_paylines()
 
 		#	load paylines
 		# load reel window, paylines 
 		payline_data = pd.read_excel(filepath, sheet_name='Paylines')
+		build_pay_table()
 
 		if(reels == 3):
 			# create virtual window:    (making up unused symbols to track values for deveopment)
-			game_window = [['gh1', 'gh4', 'gh7'], ['gh2', 'gh5', 'gh8'], ['gh3', 'gh6', 'gh9']]
+			#game_window = [['gh1', 'gh4', 'gh7'], ['gh2', 'gh5', 'gh8'], ['gh3', 'gh6', 'gh9']]
 			# meaning it's called like game_window[reel][row]
-			# note: the reels are 'horizontal' in this configuration. so reel one is the first array. 
+			build_game_window(reel1pos, reel2pos, reel3pos)
 		elif(reels == 5):
 	 		game_window = [['gh1', 'gh6', 'gh11', 'gh16', 'gh21'], ['gh2', 'gh7', 'gh12', 'gh17', 'gh22'], ['gh3', 'gh8', 'gh13', 'gh18', 'gh23'], ['gh4', 'gh9', 'gh14', 'gh19', 'gh24'], ['gh5', 'gh10', 'gh15', 'gh20', 'gh25']]
+	 		#build_game_window(reel_pos_all)
 		else:
 	 		#output logging goes here. 
 	 		print("Check settings. Choose 3 or 5 reels.")
-
+	 		quit()
 
 		#end initialization
 
@@ -79,9 +89,11 @@ class SlotMachine:
 		# bets should be negative values, wins or deposits positive
 		print("Adjusting credits at " + value)
 		self.credits = self.credits + value
+
 	def return_credits():
  		return self.credits
 
+ 	# note will need to change these inputs, to allow for >3
 	def build_game_window(reel1pos, reel2pos, reel3pos):
  		#for now: 3 reels, more logic later - like sending a list of the positions, so it's size agnostic. 
 	 # sets the window positions (reminder, game_window[row][reel])
@@ -121,12 +133,71 @@ class SlotMachine:
 
 
 	def build_pay_table():
+		#to replace with loading it in...  so in excel, 4 columns, reelNum text fields followed by the win amount
+		#accessible in object with:  paytable[each_win_line from 0 to len-1][0 to len-1 for each symbol and the value]
+		paytable = 	[
+			('W','W','W',100),
+			('B7','B7','B7',20),
+			('R7','R7','R7',20),
+			('*7','*7','*7',10),
+			('3B','3B','3B',6),
+			('2B','2B','2B',2),
+			('1B','1B','1B',1),
+			('*B','*B','*B',0.4)
+		]
 
+	def build_paylines():
+		#first payline: middle line; the 'reel positions'
+#		paylines=[(0,1),(1,1),(2,1)]
+
+		# this is what should be loaded from the excel sheet later
+		#referenced by paylines[full-payline][payline-item-number][either 0 for reel, or 1 for position]
+		#so paylines[0][2] is the third reel position to check at game_window[2][0], [2]reel three, [0]top position.
+		paylines=[
+			[(0,0),(1,0),(2,0)],
+			[(0,1),(1,1),(2,1)],
+			[(0,2),(1,2),(2,2)],
+			[(0,0),(1,1),(2,2)],
+			[(0,2),(1,1),(2,0)],
+			[(0,0),(1,1),(2,0)],
+			[(0,1),(1,2),(2,1)],
+			[(0,1),(1,0),(2,1)],
+			[(0,2),(1,1),(2,2)]
+		]
 
 	# call win determination (send each specified payline's values to check against the paytable, #operations are paylines times paytable rows)
-	def isAWin(payline):
+		# did they win? 
+		# this should cycle the payline list, (each line pulling the 3 coordinates) 
+		# then testing each of those against each paytable line
+		# ... with special consideration for wilds, denoted *
+	def isAWin():
 		#check payline from the reels, sending over the gamewindow array
 
+		# so..  for each payline, grab the symbols from the game window compare to the whole paytable win list 
+		#if it matches one of those lines (wild logic here), then pull the len-1 entry for that line
+		for line in paylines:
+			symbols=[]
+			for reel_pos in line:
+				symbols.append(game_window[reel_pos[0]][reel_pos[1]])
+			#print(str(symbols))  # this is pulling game window correctly
+ 			#test against paytable
+			for payline in paytable:
+				#print(str(payline) + " ...")
+				#from here: do the symbols for each reel spot match? so symbol[]
+				for reelnum in range(reels-1):
+					# TODO: will need to add logic for "wilds" ***
+					if(symbols[reelnum] != payline[reelnum]):
+						#if they do not match at any time, return false 
+						return false
+					else:
+						#payout is the last entry on the payline
+						adjust_credits(payline[len(payline)-1])
+						return true
+
+
+	def debug_win_jackpot():
+		# only for testing, REMOVE BEFORE ANY PRODUCTION LEVEL TESTING
+		build_game_window(2, 2, 2)
 
 	# spin reels: 
 	def spin_reels():
@@ -136,19 +207,12 @@ class SlotMachine:
 			quit()
 			#.. later: when we run out of credits, but want to test a total # of runs..  a cash injection? 
 
-
 		adjust_credits((self.bet * self.paylines * -1))
 		#STUB: remove wallet value: bet x paylines; check to see if player has enough
 
 	# 	randomly choose reel positions for each of the reels
 		randomize()
 		build_game_window(reel1pos, reel2pos, reel3pos)
-
-	def isAWin(game_window):
-		# did they win? 
-		# this should cycle the payline list, (each line pulling the 3 coordinates) 
-		# then testing each of those against each paytable line
-		# ... with special consideration for wilds, denoted *
 
 	def randomize():
 		reel1pos=random.randint(0,len(reel1)-1)
@@ -158,6 +222,7 @@ class SlotMachine:
 	  	#if(reels==5):
 	 	#	reelpos4=random.randint(0,len(reel4)-1)
 	 	#	reelpos5=random.randint(0,len(reel5)-1)
+
 
 class Simulator:
 	""" simulator class: takes in the SlotMachine class object, does stuff and tracks it. """
@@ -176,11 +241,12 @@ if __name__ == '__main__':
 	reel_total = 3
 	paylines = 9
 	bet = .25 
+	initial_credits= 1000
 	#initially, each bet is 2.25
 
 	# set the filepath - this is to be moved to the configuration file / ui input later.  literally the worst way to do this atm: 
 	filepath='/Users/jdyer/Documents/GitHub/JD-Programming-examples/Python/math_slot_simulator/PARishSheets.xlsx'
 	# build slot machine with the filepath
-	sm = SlotMachine(filepath, 3)
+	sm = SlotMachine(filepath, reel_total, paylines, bet, initial_credits)
 	# start the simulator using the slot machine. 
 
