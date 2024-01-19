41f1355204284a9e76eb65da80b97a4da45a53e6
kenfuciousd <35818192+kenfuciousd@users.noreply.github.com>
2024-01-19 11:23:14 -0800

    update
    not working, but got it half way

--- Python/AdventOfCode.com examples/AoC_day3_pt1.py
+++ Python/AdventOfCode.com examples/AoC_day3_pt1.py
@@ -14,19 +14,19 @@ def main():
 	print(len(myFile))
 #	with open(inFile) as myFile:
 	for num, line in enumerate(myFile, 0):
-		print('num is ' + str(num) )
+		#print('## num is ' + str(num) )
 		# enumerating from 0 means list-friendly counting 
 		if(num>0):
 			prevLine = myFile[num-1]
-		print('for sorting nextLine, len(myFile) looks like ' + str(len(myFile)) + ' and num is ' + str(num))
+		#print('for sorting nextLine, len(myFile) looks like ' + str(len(myFile)) + ' and num is ' + str(num))
 		if(len(myFile) > num+1):
 			nextLine = myFile[num+1]
 		else:
 			nextLine = ''
 
-		print('\n Previous Line is       ' + prevLine)
+		print('\n Previous Line is         ' + prevLine)
 		print('Current line ' + str(num) + " reading: " + line)
-		print(' Next Line is       ' + nextLine + '\n')
+		print(' Next Line is             ' + nextLine + '\n')
 
 		#tests for regex pattern matching go here -
 		digitPattern = '\d+'
@@ -37,8 +37,19 @@ def main():
 
 		foundNumbers = re.findall(digitPattern, line)
 		for foundNum in foundNumbers:
-			print("-1-1-1 found " + str(foundNum) + " at " + foundNum.start() + " to " + foundNum.end())
-			#if the found number has a symbol near it return true
+			print("-+-+-+ found " + str(foundNum) )#+ " at " + foundNum.start() + " to " + foundNum.end())
+			regexFind = re.search(foundNum, line)
+			# do we need to do error checking for digts that match/repeat?  i.e. ....390.....390....
+			#print(regexFind.span())
+			#print(regexFind.group())
+			#print(regexFind.start())
+			#print(type(regexFind.end()))
+			print(" -- at -- " + regexFind.span() + " starting at " + str(regexFind.start()) + " and ending at " + str(regexFind.end()))
+				# use the foundNum to search where it is within the line, get the start/finish
+			checkStart = regexFind.start() - 1
+			checkEnd = regexFind.end() + 1
+				# expand those numbers by +/- 1 and look for a symbolPattern within the prevLine, line, and nextLines"
+				# if true, add it to the running count
 
 			runningCount += int(foundNum)
 
