efd842a4be8f721abb070650ca385b447548dde2
kenfuciousd <35818192+kenfuciousd@users.noreply.github.com>
2024-06-18 14:32:42 -0700

    latest updates

--- Python/AdventOfCode.com examples/AoC_day3_pt1.py
+++ Python/AdventOfCode.com examples/AoC_day3_pt1.py
@@ -8,52 +8,56 @@ def main():
 	runningCount = 0
 
 	inFile = './day3_input.txt'
+#	inFile = './day3_testinput.txt'
 	myFile = open(inFile, '+r').readlines()
 	prevLine = ''
 	nextLine = ''
-	print(len(myFile))
-#	with open(inFile) as myFile:
+	#tests for regex pattern matching go here -
+	digitPattern = '\d+'
+	#symbolPattern = "!@#$%^&*()-+?_=,<>/"
+	specialString = "!\"#$%&'()*+,-/:;<=>?@[\]^_`{|}~" # removed period from the list because we want to ignore it
+
+	#print(len(myFile))
 	for num, line in enumerate(myFile, 0):
 		#print('## num is ' + str(num) )
 		# enumerating from 0 means list-friendly counting 
+		line = line.strip()
 		if(num>0):
-			prevLine = myFile[num-1]
+			prevLine = myFile[num-1].strip()
 		#print('for sorting nextLine, len(myFile) looks like ' + str(len(myFile)) + ' and num is ' + str(num))
 		if(len(myFile) > num+1):
-			nextLine = myFile[num+1]
+			nextLine = myFile[num+1].strip()
 		else:
 			nextLine = ''
 
-		print('\n Previous Line is         ' + prevLine)
-		print('Current line ' + str(num) + " reading: " + line)
-		print(' Next Line is             ' + nextLine + '\n')
-
-		#tests for regex pattern matching go here -
-		digitPattern = '\d+'
-		symbolPattern = '\p{S}'
-		#m = re.match(digitPattern, line)
-		#print (m)
-		# seems like we'd want match instead of findall... 
-
+		print('\nLast Line ' + str(num-1) + ' is\t>' + prevLine)
+		print('This Line ' + str(num) + ' is\t>' + line)
+		print('Next Line ' + str(num+1) + ' is\t>' + nextLine)
 		foundNumbers = re.findall(digitPattern, line)
 		for foundNum in foundNumbers:
-			print("-+-+-+ found " + str(foundNum) )#+ " at " + foundNum.start() + " to " + foundNum.end())
+			symbolFound = 0
+			#print("\n -+-+-+ found " + str(foundNum) )#+ " at " + foundNum.start() + " to " + foundNum.end())
 			regexFind = re.search(foundNum, line)
 			# do we need to do error checking for digts that match/repeat?  i.e. ....390.....390....
 			#print(regexFind.span())
-			#print(regexFind.group())
-			#print(regexFind.start())
-			#print(type(regexFind.end()))
-			print(" -- at -- " + regexFind.span() + " starting at " + str(regexFind.start()) + " and ending at " + str(regexFind.end()))
 				# use the foundNum to search where it is within the line, get the start/finish
-			checkStart = regexFind.start() - 1
-			checkEnd = regexFind.end() + 1
-				# expand those numbers by +/- 1 and look for a symbolPattern within the prevLine, line, and nextLines"
-				# if true, add it to the running count
+			if (regexFind.start() == 0):
+				checkStart = regexFind.start()
+			else:
+				checkStart = regexFind.start() - 1
+			if(regexFind.end() > len(line)):
+				checkEnd = regexFind.end()
+			else:
+				checkEnd = regexFind.end() + 1
+			for thisLine in (prevLine[checkStart:checkEnd], line[checkStart:checkEnd], nextLine[checkStart:checkEnd]):
+				print(thisLine)
+				if(any(c in specialString for c in thisLine)):
+					symbolFound = 1
+			if(symbolFound == 1):
+				runningCount += int(foundNum)
+				print(" !! found " + str(foundNum) + " had a symbol next to it and now runningCount is " + str(runningCount))
 
-			runningCount += int(foundNum)
-
-	print(" ++ and finally, the runningCount at the end is: " + str(runningCount))
+	print("Finally, the runningCount at the end is: " + str(runningCount))
 
 if __name__ == "__main__":
 	main()
